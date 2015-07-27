024cd48b271b1c2ed447c83a1712e9b3e650ab64
Stephane Nicoll <snicoll@pivotal.io>
2015-07-27 14:28:03 +0200

    assert number of services

--- spring-boot-testing-mock-primary/src/test/java/net/nicoll/boot/test/mock/FooServiceTests.java
+++ spring-boot-testing-mock-primary/src/test/java/net/nicoll/boot/test/mock/FooServiceTests.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.context.ApplicationContext;
 
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.core.IsEqual.*;
@@ -15,9 +16,15 @@ public class FooServiceTests extends AbstractIntegrationTests {
 	@Autowired
 	private FooService fooService;
 
+	@Autowired
+	private ApplicationContext applicationContext;
+
 	@Test
 	public void validateFoo() {
 		assertThat(fooService.something("foo"), equalTo("bar"));
+
+		// 2 BarService beans are defined
+		assertThat(this.applicationContext.getBeansOfType(BarService.class).size(), equalTo(2));
 	}
 
 }
